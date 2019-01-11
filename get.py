1f5ae8f783b9f3b9a43a11102d758d6fa9d0b7dd
Shouyang Zhou <shouyang@ualberta.ca>
2019-01-10 19:54:48 -0500

    Lab End

--- get.py
+++ get.py
@@ -1,5 +1,11 @@
 import requests
 
-r = requests.get('http://www.google.com')
+
+print(requests.__version__)
+
+r = requests.get(r'https://raw.githubusercontent.com/shouyang/CMPUT404-LAB01/master/get.py')
 
 print(r.text)
+print(r.status_code)
+
+
